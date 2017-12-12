7866cea227d15e2ffea8ba6fa3d4e7b1abb9a989
jscreve <jscreve@gmail.com>
2017-12-12 11:01:59 +0300

    add light sensor updated bluetooth sensor

--- java/majika-riello-monitoring/src/dist/lib/config/fetchData.py
+++ java/majika-riello-monitoring/src/dist/lib/config/fetchData.py
@@ -3,32 +3,27 @@ import time
 import sys
 
 name = "HC-05"      # Device name
-addr = "98:D3:31:FD:3C:05"      # Device Address
+addr = "98:D3:32:11:29:EE"      # Device Address
 port = 1         # RFCOMM port
 end = "END"
 data = ""
 received = 0
 #fetch all data
-dataType = "4"
+dataType = "1"
 # Now, connect in the same way as always with PyBlueZ
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 try :
-	s.connect((addr,port))
-	s.send(dataType);
-	received = 0;
-	while received == 0:
-		data += s.recv(64).decode('UTF-8')
-		if(end in data):
-			print (data[:-3])
-			data = ""
-			received = 1
-
-	with open('/pi/CentraleSolaireData/Programmes/arduino.txt', 'w') as mon_fichier:
-             mon_fichier.write(data)
-
+        s.connect((addr,port))
+        s.send(dataType);
+        received = 0;
+        while received == 0:
+                data += s.recv(64).decode('UTF-8')
+                if(end in data):
+                        print (data[:-3])
+                        data = ""
+                        received = 1
 
 except:
-	print("Error in socket connection")
+        print("Error in socket connection")
 finally:
-	s.close()	
-
+        s.close()
